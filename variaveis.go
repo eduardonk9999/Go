93e6f2f3560a84b6bd5f72539fb8f046a9f02260
Eduardo Silva <thiago.novak7@gmail.com>
2026-04-21 19:59:38 -0300

    exercicios

---
+++ variaveis.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+	nome := "Eduardo"
+	idade := 34
+
+	println(nome, idade)
+
+	//constantes
+	const PI = 3.14159
+
+	// Conversão de tipos
+	var i int = 42
+	var f float64 = float64(i)
+	var u uint = uint(f)
+
+	println(i, f, u)
+
+	var preco float64 = 19.90
+	println(preco)
+
+	const MAX_USUARIOS = 100
+	var usuarios = 0
+
+	print(usuarios)
+
+	var valorInicial int = 4
+	println(valorInicial)
+	var valorConvertido float64 = float64(valorInicial) / 2
+	println(valorConvertido)
+
+	var divisaoNumero uint = uint(valorConvertido)
+	println(divisaoNumero)
+}
